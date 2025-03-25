7390b6596e056415a7e3b8b653cf1521efb258aa
Azusain <azusaings@gmail.com>
2025-03-25 14:43:39 +0800

    chore: organize controller folder structure

---
+++ backend/controller/controller.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"rental-property-management-system/backend/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	// 用户注册和登录不需要管理员权限
+	r.POST("/login", Login)
+	r.POST("/register", Register)
+	r.PUT("/room", middleware.AdminRequired(), UpdateRoomInfo)
+
+	// 需要管理员权限的路由
+	r.PUT("/update-room", middleware.AdminRequired(), UpdateRoomInfo)
+	r.PUT("/admin-register", middleware.AdminRequired(), RegisterAdmin)
+	r.PUT("/get-workorder", middleware.AdminRequired(), GetWorkOrdersByAdmin)
+	r.PUT("/update-workorder", middleware.AdminRequired(), UpdateWorkOrderStatus)
+}
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"rental-property-management-system/backend/config"
+	"rental-property-management-system/backend/controller"
 	"rental-property-management-system/backend/store"
 
 	"github.com/gin-gonic/gin"
@@ -13,20 +15,26 @@ func main() {
 	if err := config.LoadConfig(); err != nil {
 		slog.Error(err.Error())
 	}
+
 	// 初始化存储层
 	if err := store.Init(); err != nil {
 		slog.Error(err.Error())
 	}
 	// 在程序结束时关闭数据库连接
 	defer store.Close()
+
 	// 迁移数据库模型
 	if err := store.MigrateModels(); err != nil {
 		slog.Error(err.Error())
 	}
+
 	// TODO: 仅用于测试
 	if err := store.GenerateMockData(); err != nil {
 		slog.Error(err.Error())
 	}
-	// 运行服务器
-	gin.Default().Run(":8080")
+
+	// 配置路由并运行服务器
+	server := gin.Default()
+	controller.SetupRoutes(server)
+	server.Run(fmt.Sprintf(":%s", config.AppConfig.Server.Port))
 }
